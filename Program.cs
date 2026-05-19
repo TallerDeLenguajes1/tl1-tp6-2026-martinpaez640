4d4612080340393d32f3d0199b542a42d6a4002a
martinpaez <martinpaez640@gmail.com>
2026-05-19 15:27:52 -0300

    Ejercicio numero 1 terminado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+Console.WriteLine("Ingrese un numero: ");
+string? entrada = Console.ReadLine();
+int i = 0, resultado = 0;
+if (int.TryParse(entrada, out i))
+{
+    if (i > 0)
+    {
+        while (i > 0)
+        {
+            resultado = resultado * 10 + i % 10;
+            i /= 10;
+        }
+        Console.WriteLine($"El resultado del numero invertido es:  {resultado}");
+    }
+    else
+    {
+        Console.WriteLine("El numero debe de ser mayor a 0");
+    }
+}else
+{
+    Console.WriteLine("Numero ingresado no valido");
+}
+
